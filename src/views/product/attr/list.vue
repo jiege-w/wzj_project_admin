daaa565ffcf45a432b1f2d692abaff93253d2cba
jiege <wuzhoujie666@gmail.com>
2021-10-24 14:20:24 +0800

    init project

---
+++ src/views/product/attr/list.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>attr</div>
+</template>
+
+<script>
+export default {
+  name: 'attr'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/product/trademark/list.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column label="序号" align="center" type="index" width="80"></el-table-column>
+      <el-table-column
+        label="品牌名称"
+        width="180">
+        <template slot-scope="scope">
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">{{ scope.row.date }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="品牌LOGO"
+        width="180">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>姓名: {{ scope.row.name }}</p>
+            <p>住址: {{ scope.row.address }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.name }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            type="warning"
+            icon="el-icon-edit"
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">修改</el-button>
+          <el-button
+            size="mini"
+            icon="el-icon-delete"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  },
+  methods: {
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    }
+  }
+}
+</script>
